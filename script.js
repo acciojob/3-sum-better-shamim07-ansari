3e1bb983964be2542a7d0824135eb3bcdc0153f0
shamim07-ansari <124763751+shamim07-ansari@users.noreply.github.com>
2023-07-15 17:42:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function threeSum(arr, target) {
-// write your code here
-  
+	arr.sort((a, b) => a - b);
+	let diff = Number.MAX_VALUE;
+	let ans = 0;
+	
+	for(let i=0; i<arr.length; i++) {
+		let start = i+1;
+		let end = arr.length-1;
+		while(start <= end) {
+			let sum = arr[i] + arr[start] + arr[end];
+			if(diff > Math.abs(sum - target)) {
+				diff = Math.abs(sum - target);
+				ans = sum;
+			}
+			else if(sum < target) start++;
+			else end--;
+		}
+	}
+	return ans;
 }
 
 module.exports = threeSum;
